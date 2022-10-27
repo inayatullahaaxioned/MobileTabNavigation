6cd5a6c809ba92218ec9e5ece058976e25adef95
inayatullahaaxioned <inayatullaha@axioned.com>
2022-10-27 15:27:48 +0530

    navigation js added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,20 @@
 /* Author: 
-
+Inayatullah
 */
 
+const image = document.querySelector(".image img"),
+  buttons = document.querySelectorAll('li'),
+  imageList = ['assets/images/Home.jpg', 'assets/images/work.jpg',
+    'assets/images/blog.jpg', 'assets/images/aboutus.jpg'];
+
+buttons.forEach(function (btn, index) {
+  btn.index = index;
+  btn.addEventListener('click', changeImage)
+})
+
+function changeImage() {
+  image.src = imageList[this.index];
+}
 
 
 
